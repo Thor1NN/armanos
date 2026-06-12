bab6ebadf5846c7c20c7c2c5eba82fff3ff269cf
Krzysztof Polak <kriss145@gmail.com>
2026-06-12 19:29:39 +0200

    Regenerate importMap with new module paths

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,10 +1,10 @@
-import { WorkoutLogsNotice as WorkoutLogsNotice_592328f487dde357ba3f861eb3783e72 } from '@/app/(payload)/admin/components/WorkoutLogsNotice'
-import { WorkoutStructureView as WorkoutStructureView_28641671c131bd2a9fa08ec2031f85f7 } from '@/app/(payload)/admin/components/WorkoutStructureView'
+import { WorkoutLogsNotice as WorkoutLogsNotice_39bf38d00355d79c704c09f118dbdf2d } from '@/app/(payload)/admin/modules/workout-logs-notice/workout-logs-notice'
+import { WorkoutStructureView as WorkoutStructureView_30ad39f13ceb101227cf63fc7340b241 } from '@/app/(payload)/admin/modules/workout-structure/workout-structure'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 /** @type import('payload').ImportMap */
 export const importMap = {
-  "@/app/(payload)/admin/components/WorkoutLogsNotice#WorkoutLogsNotice": WorkoutLogsNotice_592328f487dde357ba3f861eb3783e72,
-  "@/app/(payload)/admin/components/WorkoutStructureView#WorkoutStructureView": WorkoutStructureView_28641671c131bd2a9fa08ec2031f85f7,
+  "@/app/(payload)/admin/modules/workout-logs-notice/workout-logs-notice#WorkoutLogsNotice": WorkoutLogsNotice_39bf38d00355d79c704c09f118dbdf2d,
+  "@/app/(payload)/admin/modules/workout-structure/workout-structure#WorkoutStructureView": WorkoutStructureView_30ad39f13ceb101227cf63fc7340b241,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
