2575da1b84c65ca4d46d0b6a71721f6e9877ee65
Krzysztof Polak <kriss145@gmail.com>
2026-06-13 14:34:24 +0200

    fix: update database connection string environment variable for consistency

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -49,7 +49,7 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || '',
+      connectionString: process.env.DATABASE_URL || '',
     },
   }),
   sharp,
